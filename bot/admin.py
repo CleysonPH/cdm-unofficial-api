8b394df21b253c54cf04e6a7571ee8df1b15aeb9
Cleyson Lima <cleysonph@gmail.com>
2020-09-23 20:33:30 -0300

    Register the TelegramUser model in the admin

--- bot/admin.py
+++ bot/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import TelegramUser
+
+
+@admin.register(TelegramUser)
+class TelegramUserAdmin(admin.ModelAdmin):
+    list_display = ("chat_id", "first_name", "last_name")
+    search_fields = ("chat_id", "first_name", "last_name")
