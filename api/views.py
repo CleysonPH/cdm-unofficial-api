829647009b71dbdbe70d17d3ec0c568ae73861c4
Cleyson Lima <cleysonph@gmail.com>
2020-09-23 14:58:12 -0300

    Habilitei o campo de busca para as rotas principais

--- api/views.py
+++ api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework import status, viewsets
+from rest_framework import status, viewsets, filters
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.views import APIView
 
@@ -19,40 +19,45 @@ class MangaViewSet(viewsets.ModelViewSet):
     lookup_field = "cdm_id"
     serializer_class = MangaSerializer
     queryset = Manga.objects.all()
-    filter_backends = (DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
     filterset_fields = ("title",)
+    search_fields = ("title",)
 
 
 class AuthorViewSet(viewsets.ModelViewSet):
     lookup_field = "name"
     serializer_class = AuthorSerializer
     queryset = Author.objects.all()
-    filter_backends = (DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
     filterset_fields = ("name",)
+    search_fields = ("name",)
 
 
 class DesignerViewSet(viewsets.ModelViewSet):
     lookup_field = "name"
     serializer_class = DesignerSerializer
     queryset = Designer.objects.all()
-    filter_backends = (DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
     filterset_fields = ("name",)
+    search_fields = ("name",)
 
 
 class GenreViewSet(viewsets.ModelViewSet):
     lookup_field = "name"
     serializer_class = GenreSerializer
     queryset = Genre.objects.all()
-    filter_backends = (DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
     filterset_fields = ("name",)
+    search_fields = ("name",)
 
 
 class TypeViewSet(viewsets.ModelViewSet):
     lookup_field = "name"
     serializer_class = TypeSerializer
     queryset = Type.objects.all()
-    filter_backends = (DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend, filters.SearchFilter)
     filterset_fields = ("name",)
+    search_fields = ("name",)
 
 
 class AuthorMangas(APIView):
