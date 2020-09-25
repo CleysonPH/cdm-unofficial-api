3a1526c2e51d9c1106b34d240e62be4878c7c5db
Cleyson Lima <cleysonph@gmail.com>
2020-09-24 23:40:47 -0300

    Adicionei autenticação

--- setup/settings.py
+++ setup/settings.py
@@ -43,6 +43,7 @@ DJANGO_APPS = [
 
 THIRD_APPS = [
     "rest_framework",
+    "rest_framework.authtoken",
     "django_filters",
 ]
 
@@ -118,6 +119,12 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 50,
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ],
+    "DEFAULT_PERMISSION_CLASSES": [
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
+    ],
 }
 
 
